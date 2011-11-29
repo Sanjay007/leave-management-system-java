d162428f36b4e29f57cafbb88dcf1473c4ccc358
tranvietcuong525@gmail.com <tranvietcuong525@gmail.com@6d3ef00d-0476-b0fb-f567-d7fb613c1a12>
2011-11-29 16:25:26 +0000

    update database

--- trunk/MLS.sql
+++ trunk/MLS.sql
@@ -6,23 +6,15 @@ use MLS
 
 go
 
-create table Employee(
-	EmId int primary key,
-	EmNameLogin nvarchar(25),
-	EmPassLogin	nvarchar(8),
-	EmName nvarchar(25),
-	EmAge int,
-	EmLevel nvarchar(25),
-	EmEmail nvarchar(25),
-	EmPhone nvarchar(14),
-	EmJoinDay datetime,
-	EmDayOfLeave int
+create table MessengerType(
+	MtId int primary key,
+	MtName nvarchar(25)
 );
 
 go
 
 create table Level(
-	LvName nvarchar(25)foreign key references Employee(EmName),
+	LvName nvarchar(25),
 	LvLevel int primary key,
 	LvComment nvarchar(25),
 	LvPosition nvarchar(15)
@@ -30,10 +22,27 @@ create table Level(
 
 go
 
+
+create table Employee(
+	EmId int primary key,
+	EmNameLogin nvarchar(25),
+	EmPassLogin	nvarchar(8),
+	EmName nvarchar(25),
+	EmAge int,
+	EmLevel int constraint fk_Em foreign key references Level(LvLevel),
+	EmEmail nvarchar(25),
+	EmPhone nvarchar(14),
+	EmJoinDay datetime,
+	EmDayOfLeave int
+);
+
+
+go
+
 create table Reply(
 	ReId int,
 	MrId int,
-	EmId int,
+	EmId int constraint fk_Re foreign key references Employee(EmId),
 	ReStatus int,
 	ReDayReply datetime
 );
@@ -42,25 +51,20 @@ go
 
 create table SystemMessenger(
 	SmId int primary key,
-	MtId int,
-	EmId int,
-	SmTitle nvarchar(25)
-	SmContent nvarchar(25)
+	MtId int constraint fk_SM1 foreign key references MessengerType(MtId),
+	EmId int constraint fk_SM2 foreign key references Employee(EmId),
+	SmTitle nvarchar(25),
+	SmContent nvarchar(25),
 	SmDay datetime
 );
 
-go
 
-create table MessengerType(
-	MtId int primary key,
-	MtName nvarchar(25)
-);
 
 go
 
 create table MessengerRequest(
 	MrId int primary key,
-	EmId int,
+	EmId int constraint fk_MR foreign key references Employee(EmId),
 	MrStatus int,	
 	MrDayStart datetime,
 	MrDayEnd datetime,
@@ -68,3 +72,6 @@ create table MessengerRequest(
 	MrResoin nvarchar(60),
 	MrDayRequest datetime
 );
+
+
+insert into table
